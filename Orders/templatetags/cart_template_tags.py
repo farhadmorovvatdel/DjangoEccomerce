d1956c30b8eef27b57c05976ceaf35113ae1720c
farhad <f.morovvatdel@gmail.com>
2024-04-15 11:05:42 +0330

    comment cart_item with template tag

--- Orders/templatetags/cart_template_tags.py
+++ Orders/templatetags/cart_template_tags.py
@@ -1,11 +1,11 @@
-from django import  template
-from Orders.models import  Order
-register=template.Library()
-
-@register.filter
-def Cart_Item_Count(user):
-    if user.is_authenticated:
-        qs=Order.objects.filter(user=user,ordered=False)
-        if qs.exists():
-            return qs.first().items.count()
-    return  0
+# from django import  template
+# from Orders.models import  Order
+# register=template.Library()
+#
+# @register.filter
+# def Cart_Item_Count(user):
+#     if user.is_authenticated:
+#         qs=Order.objects.filter(user=user,ordered=False)
+#         if qs.exists():
+#             return qs.first().items.count()
+#     return  0
