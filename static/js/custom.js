cfb4c33cfd6c3e0fd4eb623d85c8698ae3467d31
farhad <f.morovvatdel@gmail.com>
2024-04-15 08:48:57 +0330

    add login authentication in ajax

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,29 +1,41 @@
 $(document).ready(function() {
-    $("#add-to-cart-btn").click(function(e) {
+       $("#add-to-cart-btn").click(function(e) {
         e.preventDefault();
-        var addToCartUrl = $(this).data('url');
-        var slug = $(this).data('slug');
-        $.ajax({
-            type: "GET",
-            url: addToCartUrl,
-            data: { slug: slug },
-            success: function(res) {
-               if (res.response === 'updated') {
-                   Swal.fire({
-                   icon: 'success',
-                   title: 'Success',
-                   text: 'Your Cart was Updated!'
-                });
-               }
-               else if(res.response ==='success'){
-                   Swal.fire({
-                   icon: 'success',
-                   title: 'Success',
-                   text: 'Item Was Add to Your Cart!'
-                });
-               }
-            },
-        });
+        var is_Authenticated =$(this).data('is-authenticated')
+        if (is_Authenticated === 'True') {
+            console.log("User is authenticated");
+             var addToCartUrl = $(this).data('url');
+             var slug = $(this).data('slug');
+            $.ajax({
+                type: "GET",
+                url: addToCartUrl,
+                data: { slug: slug },
+                success: function(res) {
+                    if (res.response === 'updated') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Your Cart was Updated!'
+                        });
+                    } else if(res.response === 'success'){
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Item Was Added to Your Cart!'
+                        });
+                    }
+                },
+            });
+        } else {
+
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning',
+                text: 'You must be logged in to your account'
+            }).then(function () {
+                $(location).attr('href', '/accounts/login/');
+            });
+        }
     });
 });
 
@@ -32,29 +44,41 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#remove-from-cart").click(function(e) {
         e.preventDefault();
-        var RemovefromCartUrl = $(this).data('url');
-        var slug = $(this).data('slug');
-        $.ajax({
-            type: "GET",
-            url: RemovefromCartUrl,
-            data: { slug: slug },
-            success: function(res) {
-               if (res.response === 'remove') {
-                   Swal.fire({
-                   icon: 'success',
-                   title: 'Success',
-                   text: 'Your Item Remove the Cart'
-                });
-               }
-               else if(res.response ==='empty'){
-                   Swal.fire({
-                   icon: 'success',
-                   title: 'Success',
-                   text: 'You Dont have active Cart!'
-                });
-               }
-            },
-        });
+        var is_Authenticated =$(this).data('is-authenticated')
+        if (is_Authenticated === 'True') {
+            var RemovefromCartUrl = $(this).data('url');
+            var slug = $(this).data('slug');
+            $.ajax({
+                type: "GET",
+                url: RemovefromCartUrl,
+                data: {slug: slug},
+                success: function (res) {
+                    if (res.response === 'remove') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Your Item Remove the Cart'
+                        });
+                    } else if (res.response === 'empty') {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'You Dont have active Cart!'
+                        });
+                    }
+
+                },
+            });
+        }
+        else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning',
+                text: 'You must be logged in to your account'
+            }).then(function () {
+                $(location).attr('href', '/accounts/login/');
+            });
+        }
     });
 });
 
@@ -64,28 +88,5 @@ $(document).ready(function() {
 
 
 
-
-
-// $(document).ready(function(){
-//     $("#add-to-cart-btn").click(function(e){
-//         alert($("#add-to-cart").val())
-//         e.preventDefault();
-//         var url = $(this).data('url');
-//         $.ajax({
-//             type: "GET",
-//             url: url,
-//             success: function(data){
-//                 if(data === 'success'){
-//                     Swal.fire({
-//                         icon: 'success',
-//                         title: 'Success',
-//                         text: 'Item added to cart successfully!'
-//                     });
-//                 }
-//             },
-//
-//         });
-//     });
-// });
 
 
