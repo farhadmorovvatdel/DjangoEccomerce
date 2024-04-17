e6ed645cbd96ea0d7ff5ee50e1d29be5494e6b5f
farhad <f.morovvatdel@gmail.com>
2024-04-17 19:58:02 +0330

    add single item add/remove in urls

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import HomeListView,ItemDetailView,Add_to_Cart,Remove_from_cart,Order_Summary,cart_item_count,Faviorate_Item
+from .views import HomeListView,ItemDetailView,\
+    Add_to_Cart,Remove_from_cart,Order_Summary\
+    ,cart_item_count,Faviorate_Item,Remove_single_item_from_cart,add_single_item_cart
 app_name='Orders'
 urlpatterns=[
     path('',HomeListView.as_view(),name='HomePage'),
@@ -9,6 +11,8 @@ urlpatterns=[
     path('remove-from-cart/<slug>',Remove_from_cart,name='remove-from-cart'),
     path('faviorate-item/<id>',Faviorate_Item,name='faviorate-item'),
     path('cart-item-count/',cart_item_count,name='cart_item_count'),
+    path('remove-single-item-from-cart/<slug>',Remove_single_item_from_cart,name='remove-single-item'),
+    path('add-single-item-to-cart/<slug>',add_single_item_cart,name='add-single-item'),
 
 
 ]
